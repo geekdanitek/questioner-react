9e69aae2f25cec15a20f051bc846d4c4e6ca4066
adedeji daniel <adedejiayokunledaniel@gmail.com>
2019-05-11 16:17:33 +0100

    165953491-task(lint): setup eslint project
    - setup eslint into project
    
    [Finishes #165953491]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.scss';
+
 class App extends Component {
-    render(){
-        return (
-            <div className="App">
-                Hello
-                <p>Questioner App</p>
-            </div>
-            
-        )
-    }
+  render() {
+    return (
+      <div className='App'>
+            Hello
+        <p>Questioner App</p>
+      </div>
+
+    );
+  }
 }
 
 export default App;

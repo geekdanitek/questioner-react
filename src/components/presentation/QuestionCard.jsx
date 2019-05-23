a14d5d414e5f969beb495f8d4953025ec246c4de
adedeji daniel <adedejiayokunledaniel@gmail.com>
2019-05-23 19:30:47 +0100

    166219386-story(profile): profile ui page
      - create profile page component
      - create questions page component
      - create question card component
      - create top question card component
    
    [Finishes #166219386]

---
+++ src/components/presentation/QuestionCard.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const QuestionCard = () => (
+  <Fragment>
+    <div className='QuestionCard'>
+      <div className='questions-card'>
+        <div className='section'>
+          <div className='section-left'>
+            <div className='vote'>
+              <button className='vote-up' />
+              <span>2423</span>
+              <button className='vote-down' />
+            </div>
+          </div>
+          <div className='section-right'>
+            <h3 className='questions-card-title'>
+                        What is the purpose of this meetup like seriously i dont understand.
+            </h3>
+            <div className='questions-card-body-content'>
+              <span className='questions-card-comments'>
+                        13 comments,
+                {' '}
+                <b>14th, Dec 2018</b>
+              </span>
+              <div className='questions-card-button-area'>
+                <Link to='/question'>
+                  <button>
+                      View Question
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='questions-card-footer'>
+          <small>The Conoil group</small>
+        </div>
+      </div>
+    </div>
+  </Fragment>
+);
+
+export default QuestionCard;

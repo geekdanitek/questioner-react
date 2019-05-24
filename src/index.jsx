b15db570d63a65d8b86380c5dde4ecc991281b7d
adedeji daniel <adedejiayokunledaniel@gmail.com>
2019-05-24 14:39:51 +0100

    166236903-task(redux): setup redux
     - setup redux into project
    
    [Finishes #166236903]

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Routes from './routes';
 import './styles/index.scss';
+import Store from './store/configure-store';
 
-ReactDOM.render(<Routes />, document.getElementById('app'));
+const store = Store();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('app')
+);
